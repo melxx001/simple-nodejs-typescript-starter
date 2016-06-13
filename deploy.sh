c8b45fbc192cb4101bc0eb5e0a2af3f2c325accb
Hicham El Hammouchi <hicham.elhammouchi@gmail.com>
2016-06-13 16:29:07 -0400

    azure 17

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,15 @@
 #!/usr/bin/env bash
 
-SCRIPT_DIR="${BASH_SOURCE[0]%\\*}"
-SCRIPT_DIR="${SCRIPT_DIR%/*}"
-ARTIFACTS=$SCRIPT_DIR/../artifacts
-DEPLOYMENT_TARGET=$ARTIFACTS/wwwroot
-
-  cd "$DEPLOYMENT_TARGET"
-  eval $NPM_CMD install
+eval $NPM_CMD install
 
 
-  eval $NPM_CMD install -g typescript
+eval $NPM_CMD install -g typescript
 
-  echo Compiling typescript code
-  eval "node_modules/typescript/bin/tsc"
-  exitWithMessageOnError "tsc failed"
+echo Compiling typescript code
+eval "node_modules/typescript/bin/tsc"
+exitWithMessageOnError "tsc failed"
 
-  eval "npm run dev"
+eval "npm run dev"
 
 
 echo "Finished successfully."
