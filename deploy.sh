47c2b636699974e49a2ebe25eb7e8a8d4fb7369c
Hicham El Hammouchi <hicham.elhammouchi@gmail.com>
2016-06-13 16:30:54 -0400

    azure 18

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-eval $NPM_CMD install
+eval "npm install"
 
 
-eval $NPM_CMD install -g typescript
+eval "npm install -g typescript"
 
 echo Compiling typescript code
 eval "node_modules/typescript/bin/tsc"
