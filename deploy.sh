0640b0705c4d590c73758e5e41c81636bcc96af4
Hicham El Hammouchi <hicham.elhammouchi@gmail.com>
2016-06-13 15:14:23 -0400

    azure test 6

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+if [ -e "$DEPLOYMENT_TARGET/package.json" ]; then
+  cd "$DEPLOYMENT_TARGET"
+  eval $NPM_CMD install --production
+  exitWithMessageOnError "npm failed"
+  cd - > /dev/null
+fi
+
+echo Compiling typescript code
+npm compile
