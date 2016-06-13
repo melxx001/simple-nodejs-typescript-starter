0e5fa6a8f12ab86da2432a1d65abf8f620f1faef
Hicham El Hammouchi <hicham.elhammouchi@gmail.com>
2016-06-13 16:48:37 -0400

    azure 20

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
 
 eval "npm install"
-
-
 eval "npm install -g typescript"
 
 echo Compiling typescript code
-eval "node_modules/typescript/bin/tsc"
-exitWithMessageOnError "tsc failed"
+eval "./node_modules/typescript/bin/tsc"
 
 eval "npm run dev"
 
