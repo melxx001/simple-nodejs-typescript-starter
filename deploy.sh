63faaa5b7e7db2f21d81b149c41820bb454b4fbb
Hicham El Hammouchi <hicham.elhammouchi@gmail.com>
2016-06-13 15:18:45 -0400

    azure test 7

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 if [ -e "$DEPLOYMENT_TARGET/package.json" ]; then
   cd "$DEPLOYMENT_TARGET"
-  eval $NPM_CMD install --production
+  eval $NPM_CMD install --development
   exitWithMessageOnError "npm failed"
   cd - > /dev/null
 fi
