3830961d27b39991312263f4b6c85844b8728da0
Hicham El Hammouchi <hicham.elhammouchi@gmail.com>
2016-06-13 15:45:00 -0400

    azure 11

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+SCRIPT_DIR="${BASH_SOURCE[0]%\\*}"
+SCRIPT_DIR="${SCRIPT_DIR%/*}"
+ARTIFACTS=$SCRIPT_DIR/../artifacts
+DEPLOYMENT_TARGET=$ARTIFACTS/wwwroot
+
 if [ -e "$DEPLOYMENT_TARGET/package.json" ]; then
   cd "$DEPLOYMENT_TARGET"
   eval $NPM_CMD install --development
